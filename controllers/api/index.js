48e877b8ea91a1e40fc25725ba443c2ddcb8d916
Ben S <benslinde@gmail.com>
2021-09-27 11:07:49 -0400

    added function to add a new post from profile

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,13 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 
+//added for my post
+const postRoutes = require('./postRoutes');
+
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+
+//added for my post
+router.use('/posts', postRoutes);
 
 module.exports = router;
