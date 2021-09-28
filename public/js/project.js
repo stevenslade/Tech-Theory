7b0298ab342bd4fdb3e576f4cd1ce99bc6ff464d
Ben S <benslinde@gmail.com>
2021-09-28 09:26:45 -0400

    WIP, continue building comments feature

--- public/js/project.js
+++ public/js/project.js
@@ -1,26 +1,25 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/projects/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete project');
-      }
-    }
-  };
+  event.preventDefault();
 
-}
-  
-  
+  const description = document.querySelector('#comment-desc').value.trim();
+
+  if (description) {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({ description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/comment');
+    } else {
+      alert('Failed to create comment');
+    }
+  }
+};
+
 document
-   .querySelector('.project-list')
-   .addEventListener('click', delButtonHandler);
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newFormHandler);
