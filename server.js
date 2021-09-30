f011d7c5b973c54570c9452d5fba175b7cbdee8e
Ben S <benslinde@gmail.com>
2021-09-30 11:42:40 -0400

    commented out the requiredotenv in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 //just added
-require('dotenv').config();
+//require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -16,8 +16,6 @@ const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
-
-
 
 //The addition of rolling:true and maxAge was meant to cause the cookie to expire after idle and it is working, 30000 equals 30 seconds, 300000 equals five minutes.  But nothing happens after the time interval, its just that if you try to do something it takes you back to a login screen
 const sess = {
@@ -38,6 +36,9 @@ app.use(session(sess));
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+// app.engine(".hbs", exphbs({ extname: ".hbs" }));
+// app.set("view engine", ".hbs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
